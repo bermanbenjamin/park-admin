c33e04c65380c87b3ec0d2ae151a06b9fc2b082d
Benja <bermanschultz29@gmail.com>
2022-05-04 15:24:47 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import jsonServerProvider from 'ra-data-json-server';
+import * as React from "react";
+import { Admin, ListGuesser, Resource } from 'react-admin';
+import { DriversPage } from "./pages/Drivers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => (
+      <Admin dataProvider={dataProvider}>
+          <Resource name="posts" list={ListGuesser} create={DriversPage} />
+          <Resource name="users" list={ListGuesser}/>
+      </Admin>
   );
-}
+  
 
 export default App;
---
+++ src/pages/Drivers/index.js
@@ -0,0 +1,15 @@
+export function DriversPage() {
+  return (
+    <div>
+      <h1>Drivers</h1>
+    </div>
+  );
+}
+
+export function DriversList(){
+    return (
+        <div>
+        <h1>Drivers List</h1>
+        </div>
+    );
+}
