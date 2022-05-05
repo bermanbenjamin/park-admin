c25c22093e91b0249b903a3c3b464a2d5cfebc84
Benja <bermanschultz29@gmail.com>
2022-05-05 00:51:04 -0300

    forms to car create

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import jsonServerProvider from 'ra-data-json-server';
 import * as React from "react";
 import { Admin, ListGuesser, Resource } from 'react-admin';
+import { CarPage } from './pages/Car';
 import { DriversPage } from "./pages/Drivers";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
       <Admin dataProvider={dataProvider}>
-          <Resource name="posts" list={ListGuesser} create={DriversPage} />
-          <Resource name="users" list={ListGuesser}/>
+          <Resource name="cliente" list={ListGuesser} create={DriversPage} />
+          <Resource name="estacionamento" list={ListGuesser}/>
+          <Resource name="veiculo" list={ListGuesser} />
+          <Resource name="users" list={ListGuesser} create={CarPage}/>
       </Admin>
   );
   
